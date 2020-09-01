766c3f6d3ddf3d7f71f8fa06a57d138c84f3088a
Jesse Wood <woodRock@users.noreply.github.com>
2020-09-01 12:13:27 +1200

    Update main.js

--- src/webapp/main.js
+++ src/webapp/main.js
@@ -1,12 +1,15 @@
 const mapfilePath = "/home/woodj/Documents/solid-waffle/src/mapfile/nz-coastlines-topo.map";
+
 const accessToken = 'pk.eyJ1Ijoid29vZHJvY2siLCJhIjoiY2s5MTN0NmoyMDhocjNmbm5tbnp3bXRieCJ9.OPqxdTBIE5QBAgj3NuuLSA';
+
 const zoom = 16;
+
 const wellington = {
   latitude: -41.346273,
   longitude: 174.776097
 };
 
-var basemaps = {
+const basemaps = {
     Coastlines: L.tileLayer.wms('http://localhost/cgi-bin/mapserv?map='+mapfilePath, {
         version: '1.1.1',
         format: 'image/png',
@@ -40,16 +43,15 @@ const overlayMaps = {
   Land: basemaps.Land
 };
 
-var crs = L.CRS.EPSG4326;
+const crs = L.CRS.EPSG4326;
 
-var map = L.map('mapid', {
+const map = L.map('mapid', {
   crs: crs
 }).setView([wellington.latitude, wellington.longitude], zoom);
 
 L.control.layers(null, overlayMaps).addTo(map);
 L.control.scale().addTo(map);
 
-// basemaps.Topography.addTo(map);
 basemaps.Coastlines.addTo(map);
 basemaps.Sea.addTo(map);
 basemaps.Land.addTo(map);
